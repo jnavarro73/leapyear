3d266e8b5c9067f3ea99c7bf1a26d21e1ce44166
jjjkkk <jjjkkk@jjj>
2018-06-18 18:05:16 +0200

    //working but some bootstrap and Error catching are not full enabled

--- resources/views/testLeap/index.blade.php
+++ resources/views/testLeap/index.blade.php
@@ -2,38 +2,49 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta  http-equiv="expires" content="0" />
-    <script src="{{asset('js/jquery-3.1.1.min.js')}}"></script>
-    <link rel="stylesheet" href="{{asset('css/bootstrap-theme.min.css')}}">
-    <script src=" {{ asset('js/extraDateFunctions.js') }}"></script>
-    <div id='container' class="container">
-        <div class=" text-center" class="col-md-12">
-            <h1>Test Hoy-voy </h1>
+    <head>
+        <link  href="{{asset('css/bootstrap-theme.min.css')}}" rel="stylesheet">
+        <title>Test Hoy-voy </title>   
+
+    </head>
+    <body>
+        <script src="{{asset('js/jquery-3.1.1.min.js')}}"></script>
+        <script src=" {{ asset('js/bootstrap.min.js') }}"></script>
+        <script src=" {{ asset('js/extraDateFunctions.js') }}"></script>
+
+
+        <div class=" text-center" >
+            <h1>Test hoy-voy.com </h1>
         </div>
+        <div id="wrap">
+            <div id='container' class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        ¿Pertenece a año bisiesto esta fecha?:{{testLeapController::FECHA}}
+                        <input type='hidden' id='datesent' value='{{testLeapController::FECHA}}'>
+                    </div>
+                </div>
 
-        <div class="row">
-            <div class="col-md-12">
-                ¿Pertenece a año bisiesto esta fecha?:{{testLeapController::FECHA}}
-                <input type='hidden' id='datesent' value='{{testLeapController::FECHA}}'>
-            </div>
+                <div class="row">            
+                    <div class="row">
+                        <div class="alert alert-success collapse" role="alert">
+
+                        </div>
+                        <div class="alert alert-info collapse" role="alert">
+
+                        </div>
+                        <div class="alert alert-warning collapse" role="alert">
+
+                        </div>
+                        <div class="alert alert-danger collapse" role="alert">
+
+                        </div>
+                    </div>
+                    <div id="content" name="content">
+
+                    </div>
+                </div>
+            </div>  
         </div>
-        
-        <div class="row">            
-            <div class="row">
-                <div class="alert alert-success collapse" role="alert">
-                  
-                </div>
-                <div class="alert alert-info collapse" role="alert">
-
-                </div>
-                <div class="alert alert-warning collapse" role="alert">
-
-                </div>
-                <div class="alert alert-danger collapse" role="alert">
-
-                </div>
-            </div>
-            <div id="content" name="content">
-
-            </div>
-        </div>
+    </body>     
 </html>
